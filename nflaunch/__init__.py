72c839e5c2e5daaeb175137d15d34a46a244da35
Rodrigo JC <rodrigo.jara.calo@gmail.com>
2025-11-19 01:25:29 +0100

    release v0.1.0

--- nflaunch/__init__.py
+++ nflaunch/__init__.py
@@ -4,7 +4,7 @@ A command-line tool designed to simplify the launching of Nextflow pipelines
 on cloud batch services.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.0"
 
 from nflaunch.cli.main import main
 
